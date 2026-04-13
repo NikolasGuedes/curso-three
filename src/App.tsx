dcff9bb65a6e197514d1e653cec5a6f53de6286d
Nikolas Guedes da Silva <guedesnikolas@gmail.com>
2026-04-13 20:10:36 -0300

    fist Scene

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import './App.css'
+import "./App.css";
 import { Canvas } from "@react-three/fiber";
-import  ThreeElement  from './ThreeElement';
+import ThreeElement from "./ThreeElement";
+import { OrbitControls } from "@react-three/drei";
 
 function App() {
   return (
     <>
-    <Canvas>
-      <ThreeElement />
-    </Canvas>
+      <Canvas
+        camera={{
+          fov: 75,
+          near: 1,
+          far: 100,
+          position: [3, 3, 0],
+        }}
+      >
+        <ThreeElement />
+        <OrbitControls
+          enableZoom={false}
+          enablePan={false}
+          // minAzimuthAngle={-Math.PI / 4}
+          // maxAzimuthAngle={Math.PI / 4}
+          // minPolarAngle={Math.PI / 6}
+          // maxPolarAngle={Math.PI - Math.PI}
+        />
+        <axesHelper args={[5]} />
+        <gridHelper args={[10, 10, "red", "blue"]} />
+      </Canvas>
     </>
   );
 }
--- src/ThreeElement.tsx
+++ src/ThreeElement.tsx
@@ -1,10 +1,21 @@
 import * as THREE from "three";
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
 
 export default function ThreeElement() {
+  const boxRef = useRef<THREE.Mesh>(null);
+
+  useFrame((_, delta) => {
+    if (boxRef.current) {
+      boxRef.current.rotation.y += delta;
+    }
+  });
+
   return (
     <>
       <directionalLight position={[5, 5, 5]} />
       <mesh
+        ref={boxRef}
         rotation={[
           THREE.MathUtils.degToRad(45),
           THREE.MathUtils.degToRad(90),
